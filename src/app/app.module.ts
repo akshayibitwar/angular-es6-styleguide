1e5482ac6a32d8a7f1bd67436aabfaa7f726179f
akshay <akshayebitwar@gmail.com>
2025-02-21 15:31:28 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './shared/component/comp1/comp1.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     Comp1Component,
+    TestComponent,
     
   ],
   imports: [
